0ee564e20cd3deed695d1e459cfbf122f39be077
Rahaf <esrrahaf.ase@gmail.com>
2026-04-17 16:32:15 +0300

    Fix Streamlit secrets config

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_secret(key):
+    if key in st.secrets:
+        return st.secrets[key]
+    return os.getenv(key)
+
 # Elm / Nuha API settings
-ELM_API_URL = os.getenv("ELM_API_URL")
-ELM_API_KEY = os.getenv("ELM_API_KEY")
-ELM_MODEL   = os.getenv("ELM_MODEL")
+ELM_API_URL = get_secret("ELM_API_URL")
+ELM_API_KEY = get_secret("ELM_API_KEY")
+ELM_MODEL   = get_secret("ELM_MODEL")
 
 # Embedding model
 EMBEDDING_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
